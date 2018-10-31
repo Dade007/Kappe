5feead2e097e7de0f683822a4c286b8c6c811e4f
GrigoryP <grigarash.fan@yandex.ru>
2018-10-31 21:47:45 +0400

    page_3

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ $(function() {
 	$('.info').click(function() {
 		$('.info-box').toggleClass('open');
 	});
+
+	$('.line-b_color').each(function () {
+		let width = $(this).data('skill_script');
+
+		$(this).css('width', width + '%');
+	});
+
 });
